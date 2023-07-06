74bae155b61702597dabf0d6707145bca9d939c3
shinkared <valentin99181@gmail.com>
2023-07-06 14:42:24 +0300

    Добавлены комментарии

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import sqlite3
 
 
 def bd_ini(stores: list):
+    # Подключение к БД
     try:
         sqlite_connection = sqlite3.connect('kfc.db')
         cursor = sqlite_connection.cursor()
+        # Запрос создания таблицы
         create_t = f"""CREATE TABLE IF NOT EXISTS kfc_nsk (
     storeId INTEGER PRIMARY KEY,
     title_en TEXT,
@@ -19,6 +21,7 @@ def bd_ini(stores: list):
     breakfastStartTime TIME,
     breakfastEndTime TIME);"""
         cursor.execute(create_t)
+        # Запись в ресторанов в БД
         for i in stores:
             write_to_bd(cursor, i["store"])
         sqlite_connection.commit()
@@ -32,6 +35,7 @@ def bd_ini(stores: list):
 
 
 def write_to_bd(cursor, store):
+    # Составление запроса и последующая запись
     query = f"""INSERT OR IGNORE INTO kfc_nsk (storeId, title_en, title_ru, streetAddress, city, coordinates_x, 
     coordinates_y, startTimeLocal, endTimeLocal, breakfastStartTime, breakfastEndTime) VALUES (
     {store["storeId"]},
@@ -49,6 +53,7 @@ def write_to_bd(cursor, store):
 
 
 def main():
+    # Составление запроса
     body = {"coordinates": [55.004174, 82.894257], "radiusMeters": 130000, "channel": "website", "showClosed": True}
     headers = {"Content-Length": '155', "Host": "api.kfc.digital"}
     response = requests.post('https://api.kfc.digital/api/store/v2/store.geo_search', headers=headers, json=body)
@@ -57,7 +62,6 @@ def main():
         stores = data["searchResults"]
         bd_ini(stores)
     print(response.status_code)
-
 
 
 if __name__ == '__main__':
